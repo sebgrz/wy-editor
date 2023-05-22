dd4b4881959c76c05de38fec6736d2d25b51b3e1
Seba G <sebag.git@gmail.com>
2023-05-22 21:21:39 +0200

    wip: set bold modifier

--- src/lib/Editor/hooks/useModifiers.ts
+++ src/lib/Editor/hooks/useModifiers.ts
@@ -1,19 +1,40 @@
 import { CursorPosition, Modifier, ModifierType } from "..";
-import { FormattingEvent } from "../controllers/FormattingController";
+import { FormattingEvent, FormattingEventType } from "../controllers/FormattingController";
 
 type ModifierData = Map<number, Map<ModifierType, Modifier>>;
 
-type ModifierActionFunc = (line: number, position: number, event: FormattingEvent) => boolean; 
-type ModifierSelectionActionFunc = (startPosition: CursorPosition, endPosition: CursorPosition, event: FormattingEvent) => boolean; 
+type ModifierActionFunc = (line: number, position: number, event: FormattingEvent) => boolean;
+type ModifierSelectionActionFunc = (startPosition: CursorPosition, endPosition: CursorPosition, event: FormattingEvent) => boolean;
 type LineModifiersFunc = (line: number) => Modifier[];
+type PositionModifiersFunc = (line: number, position: number) => Modifier[];
 
-type ModifiersResult = [ action: ModifierActionFunc, selectionAction: ModifierSelectionActionFunc, line: LineModifiersFunc];
+type ModifiersResult = [
+  action: ModifierActionFunc, 
+  selectionAction: ModifierSelectionActionFunc,
+  lineModifiers: LineModifiersFunc,
+  positionModifiers: PositionModifiersFunc
+];
 
 
 const modifiers: ModifierData = new Map();
 
 const useModifiers = (): ModifiersResult => {
   const modifierAction: ModifierActionFunc = (line: number, position: number, event: FormattingEvent) => {
+    let type = (() => {
+      switch (event.type) {
+        case FormattingEventType.BOLD: {
+          return event.isActive ? ModifierType.START_BOLD : ModifierType.END_BOLD;
+        }
+      }
+    })()
+
+    if (type !== undefined) {
+      if (modifiers.get(line)?.set(type, { position: position, type: type }) === undefined) {
+        let modifiersLine = new Map<ModifierType, Modifier>();
+        modifiersLine.set(type, { position: position, type: type });
+        modifiers.set(line, modifiersLine);
+      }
+    }
     return false;
   }
 
@@ -21,11 +42,15 @@ const useModifiers = (): ModifiersResult => {
     return false;
   }
 
-  const lineModifiers: LineModifiersFunc = (line: number) => {
+  const lineModifiers: LineModifiersFunc = (line: number) => {    
     return [];
   }
 
-  return [ modifierAction, modifierSelectionAction, lineModifiers ];
+  const positionModifiers: PositionModifiersFunc = (line: number, position: number) => {
+    return [];
+  }
+
+  return [modifierAction, modifierSelectionAction, lineModifiers, positionModifiers];
 }
 
 export default useModifiers;
